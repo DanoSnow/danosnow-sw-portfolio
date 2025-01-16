44d7cb741866b548f37dceb622569a3308db1900
Daniel <izaguirredaniel9712@gmail.com>
2025-01-16 15:22:10 -0500

    Portfolio desing set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { createContext, useState } from "react"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Skills from "./Pages/Skills";
+import Resume from "./Pages/Resume";
+
+interface RouteContextType {
+  currentRoute: string;
+  setCurrentRoute: (route: string) => void;
+}
+
+export const RouteContext = createContext<RouteContextType | undefined>(undefined);
+
 function App() {
+  const [currentRoute, setCurrentRoute] = useState<string>("/");
+
   return (
-    <>
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello world!
-      </h1>
-    </>
+    <RouteContext.Provider value={{ currentRoute, setCurrentRoute }}>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/danosnow-sw-portfolio" element={<Home />} />
+            <Route path="/danosnow-sw-portfolio/about" element={<About />} />
+            <Route path="/danosnow-sw-portfolio/skills" element={<Skills />} />
+            <Route path="/danosnow-sw-portfolio/resume" element={<Resume />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </RouteContext.Provider >
   )
 }
 
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
+import { RouteContext } from "../App"
+
+const Navbar = () => {
+    const routeContext = useContext(RouteContext);
+    const location = useLocation();
+
+    useEffect(() => {
+        routeContext?.setCurrentRoute(location.pathname);
+    }, [location.pathname, routeContext?.currentRoute]);
+
+    return (
+        <div className="navbar bg-slate-800 bg-opacity-70 fixed z-10">
+            <div className="navbar-start"></div>
+            <div className="navbar-center">
+                <div className='flex md:space-x-16 space-x-8 text-lg main-font'>
+                    <Link to="/danosnow-sw-portfolio" className={routeContext?.currentRoute === '/danosnow-sw-portfolio' ? 'text-red-500' : 'text-white'}>Home</Link>
+                    <Link to="/danosnow-sw-portfolio/about" className={routeContext?.currentRoute === '/danosnow-sw-portfolio/about' ? 'text-red-500' : 'text-white'}>About</Link>
+                    <Link to="/danosnow-sw-portfolio/skills" className={routeContext?.currentRoute === '/danosnow-sw-portfolio/skills' ? 'text-red-500' : 'text-white'}>Skills</Link>
+                    <Link to="/danosnow-sw-portfolio/resume" className={routeContext?.currentRoute === '/danosnow-sw-portfolio/resume' ? 'text-red-500' : 'text-white'}>Resume</Link>
+                </div>
+            </div>
+            <div className="navbar-end"></div>
+        </div>
+    )
+}
+
+export default Navbar
